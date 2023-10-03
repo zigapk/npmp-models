c56dc81459f0dcf277341a5f128fc386bd43628c
Miha Moškon <miha.moskon@fri.uni-lj.si>
2023-10-03 11:12:30 +0200

    ...

--- counters/params.py
+++ counters/params.py
@@ -1,10 +1,11 @@
+# units in 
 param_ranges = {"transcription": {"min": 0.01, "max": 50},   
                 "translation": {"min": 0.01, "max": 50},  
                 "protein_production": {"min": 0.1, "max": 50},                          
                 "rna_degradation": {"min": 0.1, "max": 100},        
                 "protein_degradation": {"min": 0.001, "max": 50},         
-                "hill": {"min": 1, "max": 5},         
-                "Kd": {"min": 0.01, "max": 250}, 
+                "hill": {"min": 1, "max": 5},   # n         
+                "Kd": {"min": 0.01, "max": 250}, # dissociation constant
                 "protease_concentration": {"min": 10, "max":1000},
                 "leak": 0.1,}    
 
